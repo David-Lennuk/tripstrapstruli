20a5e491f4ac19e5eacdf39031c79996074ee673
David-Lennuk <david.nikolajev07@gmail.com>
2025-02-18 10:47:53 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lumememm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ StartPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace Lumememm;
+
+public partial class StarPage : ContentPage
+{
+    public List<ContentPage> lehed = new List<ContentPage>() { new DateTimePage(1), new StepperSliderPage(1), new RGB_mudeliPage(1) };
+    public List<string> tekstid = new List<string> { "Time", "Slider", "RGB" };
+
+    ScrollView sv;
+    VerticalStackLayout vsl;
+
+    public StarPage()
+    {
+        Title = "Avaleht";
+        vsl = new VerticalStackLayout { BackgroundColor = Color.FromArgb("#006600") };
+
+        for (int i = 0; i < tekstid.Count; i++)
+        {
+            Button nupp = new Button
+            {
+                Text = tekstid[i],
+                BackgroundColor = Color.FromArgb("#ffa31a"),
+                TextColor = Color.FromArgb("#006600"),
+                BorderWidth = 10,
+                ZIndex = i,
+                FontFamily = "Lovesive 400",
+                FontSize = 30
+            };
+            vsl.Add(nupp);
+            nupp.Clicked += Lehte_avamine;
+        }
+
+
+        sv = new ScrollView { Content = vsl };
+        Content = sv;
+    }
+
+    private async void Lehte_avamine(object? sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        await Navigation.PushAsync(lehed[btn.ZIndex]);
+    }
+
+    private async void Tagasi_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
+}
